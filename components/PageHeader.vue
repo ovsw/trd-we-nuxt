68a45042a00b0e4a27b4e37282c4257b7a725220
Ovi Savescu <ovi@ovswebsites.com>
2021-04-30 15:01:46 +0300

    page header fixes

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -3,16 +3,26 @@
     <div class="spotlight-gradient"></div>
     <div class="max-w-7xl mx-auto py-16 px-4 sm:py-36 sm:px-6 lg:px-8">
       <div class="text-center">
-        <h1
-          class="text-base font-semibold text-b-orange opacity-70 tracking-widest uppercase"
-        >
-          {{ title }}
+        <h1>
+          <span
+            class=" block text-base font-semibold text-b-orange opacity-70 tracking-widest uppercase"
+          >
+            <template v-if="longTitle != ''">
+              {{ title }}
+            </template>
+            <template v-else>
+              Tridia
+            </template>
+          </span>
+          <span
+            class="block mt-5 text-4xl font-extrabold text-gray-100 sm:text-5xl sm:tracking-tight lg:text-6xl max-w-5xl mx-auto"
+          >
+            <span v-if="longTitle != ''">
+              {{ longTitle }}
+            </span>
+            <span v-else>{{ title }}</span>
+          </span>
         </h1>
-        <h2
-          class="mt-5 text-4xl font-extrabold text-gray-100 sm:text-5xl sm:tracking-tight lg:text-6xl max-w-5xl mx-auto"
-        >
-          {{ longTitle }}
-        </h2>
         <p :v-if="intro" class="max-w-xl mt-5 mx-auto text-xl text-gray-500">
           {{ intro }}
         </p>
@@ -30,7 +40,7 @@ export default {
     },
     longTitle: {
       type: String,
-      default: "missing long title"
+      default: ""
     },
     intro: {
       type: String
